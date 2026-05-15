b03d1e7447ab6c1b71009df37dee0c3bbfd4d053
Muhammad Nura Sarki <kingalameen0000@gmail.com>
2026-05-15 20:58:09 +0100

    Add migration to add lga_id column to events table

---
+++ database/migrations/2026_05_15_add_lga_id_to_events_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            if (!Schema::hasColumn('events', 'lga_id')) {
+                $table->foreignId('lga_id')->nullable()->after('location')->constrained('lgas')->onDelete('set null');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->dropForeignIdFor('lgas');
+            $table->dropColumn('lga_id');
+        });
+    }
+};
