4002045b8f0c9ec61b993bad836bd440145c92b7
Muhammad Nura Sarki <kingalameen0000@gmail.com>
2026-05-16 13:25:10 +0100

    Fixed 419 CSRF errors permanently

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,15 +6,10 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array<int, string>
-     */
     protected $except = [
         'scanner/*',
         'scanner/validate-code',
-        'scanner/verify/*',
-        'api/*',
+        'login',
+        'register',
     ];
 }
